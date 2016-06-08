66e9bdd608402ba1d09ca6c264fcdf5fc098cf9c
yqu1 <quyaoxian@gmail.com>
2016-06-08 14:03:13 -0400

    some more functionality

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,11 +2,29 @@
 
 angular.module('schedulerApp', [])
 
-.controller('pageController', ['$scope', function($scope){
+.controller('scheduleController', ['$scope', 'scheduleFactory', function($scope, scheduleFactory){
 	$scope.tab = 1;
+	$scope.schedules = scheduleFactory;
+	$scope.filtText = "Mon";
 
 	$scope.select = function(setTab){
 		$scope.tab = setTab;
+
+		if($scope.tab === 1) {
+			$scope.filtText = "Mon";
+		}
+		if($scope.tab === 2) {
+			$scope.filtText = "Tue";
+		}
+		if($scope.tab === 3) {
+			$scope.filtText = "Wed";
+		}
+		if($scope.tab === 4) {
+			$scope.filtText = "Thu";
+		}
+		if($scope.tab === 5) {
+			$scope.filtText = "Fri";
+		}
 	}
 
 	$scope.isSelected = function(checkTab){
@@ -25,4 +43,6 @@ angular.module('schedulerApp', [])
 		};
 	}
 
+
 }])
+
---
+++ app/scripts/services.js
@@ -0,0 +1,40 @@
+'use strict'
+
+angular.module('schedulerApp')
+.factory('scheduleFactory', function(){
+
+	var schedules = [
+			{
+				day:"Mon",
+				name: "math",
+				time: "3:00 - 4:00",
+				instructor: "Qu Yaoxian",
+				location: "your mom's vagina"
+			},
+			{
+				day:"Mon",
+				name: "CS",
+				time: "5:00 - 6:00",
+				instructor: "Qu Yaoxian",
+				location: "your mom's vagina"
+			},
+
+			{
+				day:"Tue",
+				name: "CS",
+				time: "3:00 - 4:00",
+				instructor: "Qu Yaoxian",
+				location: "toilet"
+			},
+			{
+				day:"Tue",
+				name: "math",
+				time: "5:00 - 6:00",
+				instructor: "Qu Yaoxian",
+				location: "your mom's vagina"
+			}
+	]
+
+	return schedules;
+
+})
